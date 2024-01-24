b725cde39c17defd849f66830f244ba776af5bfa
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2024-01-24 14:36:50 +0100

    work

--- groupWebsite/script.js
+++ groupWebsite/script.js
@@ -4,28 +4,26 @@ document.querySelector('.login-form').addEventListener('submit', function(event)
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-
     document.querySelector('.login-container').innerHTML = '';
 
-    // wachtwoord kiezen daar beneden
-    if (password === 'yourPassword') {
+
+    const passwords = {
+        'password1': 'https://website1.com',
+        '69': 'https://mauricewa.github.io/PortfolioPiPe/',
+        'password3': 'https://website3.com'
+    };
+
+
+    if (password in passwords) {
         let welcomeMessage = document.createElement('div');
         welcomeMessage.innerHTML = "<h2>Welcome " + username + "! to the new room</h2>";
         document.querySelector('.login-container').appendChild(welcomeMessage);
 
-        var links = [
-            {url: 'http://website1.com', text: 'Website 1'},
-            {url: 'http://website2.com', text: 'Website 2'},
-            {url: 'http://website3.com', text: 'Website 3'},
-            {url: 'http://website4.com', text: 'Website 4'}
-        ];
 
-        links.forEach(function(link) {
-            let btn = document.createElement('button');
-            btn.innerHTML = link.text;
-            btn.onclick = function() { window.location.href = link.url; };
-            document.querySelector('.login-container').appendChild(btn);
-        });
+        let btn = document.createElement('button');
+        btn.innerHTML = 'Visit Website';
+        btn.onclick = function() { window.location.href = passwords[password]; };
+        document.querySelector('.login-container').appendChild(btn);
     } else {
         alert('Incorrect password');
     }
